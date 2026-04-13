bff9d5e018e56a879f834006301025a2714fd4d3
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-13 13:30:43 -0300

    ex07

---
+++ Lista00/ex07/Ex07.java
@@ -0,0 +1,20 @@
+package Lista00.ex07;
+
+public class Ex07 {
+
+    public static void main(String args[]) {
+
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();
+
+    for(int i = 1; i <= 10; i++) {
+        System.out.println("Tabuada do " + i);
+
+        for(int j = 1; j <= 10; j++) {
+            int resultado = i * j;
+            System.out.println(i + " x " + j + " = " + resultado);
+        }
+        System.out.println();
+        }
+    }
+}
