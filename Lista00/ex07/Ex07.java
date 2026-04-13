4f8bb0a9faf7bbef3a24bec7c6ddf41abae41870
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-13 13:38:12 -0300

    corrigindo ex07

--- Lista00/ex07/Ex07.java
+++ Lista00/ex07/Ex07.java
@@ -11,8 +11,7 @@ public class Ex07 {
         System.out.println("Tabuada do " + i);
 
         for(int j = 1; j <= 10; j++) {
-            int resultado = i * j;
-            System.out.println(i + " x " + j + " = " + resultado);
+            System.out.println(i + " x " + j + " = " + (i * j));
         }
         System.out.println();
         }
