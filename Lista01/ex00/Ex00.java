0b0fb19d961d61b5f3b6bf4a79a49d117a2bbc2f
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-18 22:43:40 -0300

    ex00

---
+++ Lista01/ex00/Ex00.java
@@ -0,0 +1,27 @@
+package Lista01.ex00;
+
+import java.util.Scanner;
+
+public class Ex00 {
+
+    public static void main(String args[]) {
+
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite um número inteiro: ");
+    int numero = scanner.nextInt();
+
+    if(numero % 2 == 0) {
+        System.out.println("O número " + numero + " é par.");
+    } else {
+        System.out.println("O número " + numero + " é ímpar.");
+    }
+
+    scanner.close();
+    }
+    
+
+}
