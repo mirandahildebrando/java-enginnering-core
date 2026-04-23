a75ae5945ad5212a39a00619f4e67eaf80839bd3
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-23 13:35:03 -0300

    ex07

---
+++ Lista01/ex07/Ex07.java
@@ -0,0 +1,15 @@
+package Lista01.ex07;
+
+public class Ex07 {
+
+    public static void main (String args[] ) {
+
+System.out.print("\033[H\033[2J");  
+System.out.flush();
+
+        int a = 12345;
+
+        System.out.print( String.valueOf(a).length() + " digitos");
+    }
+
+}
