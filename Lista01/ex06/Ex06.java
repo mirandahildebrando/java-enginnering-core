41d3b51131876811e9fb34e70fc377da888eac2e
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-23 13:31:03 -0300

    ex06

---
+++ Lista01/ex06/Ex06.java
@@ -0,0 +1,18 @@
+package Lista01.ex06;
+
+public class Ex06 {
+
+    public static void main (String args[] ) {
+
+System.out.print("\033[H\033[2J");  
+System.out.flush();
+
+        int a = 1234;
+
+        for(int i = 4; i >= 1; i--) {
+
+            System.out.print(i);
+        } 
+    }
+
+}
