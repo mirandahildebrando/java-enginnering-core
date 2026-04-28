ee54c3fdff76d963777af21732b4dcbf5b31406c
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-28 13:32:21 -0300

    ex10

--- Lista01/ex09/Ex09.java
+++ Lista01/ex09/Ex09.java
@@ -1,6 +1,9 @@
 public class Ex09 {
 
         public static void main (String[] args) {
+
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();
         int numeroOriginal = 121;
         
         boolean resultado = ePalindromo(numeroOriginal);
---
+++ Lista01/ex10/Ex10.java
@@ -0,0 +1,47 @@
+package Lista01.ex10;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+    public static void main(String args[] ) {
+
+System.out.print("\033[H\033[2J");  
+System.out.flush();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número inteiro : ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número inteiro : ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Digite o terceiro número inteiro : ");
+        int num3 = scanner.nextInt();
+
+        int temp;
+
+        if(num1 > num2) {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        if(num1 > num3) {
+            temp = num1;
+            num1 = num3;
+            num3 = temp;
+        }
+
+        if(num2 > num3) {
+            temp = num2;
+            num2 = num3;
+            num3 = temp;
+        }
+
+        System.out.println("Números em ordem crescente : " + num1 + " " + num2 + " " + num3);
+
+    }
+
+}
